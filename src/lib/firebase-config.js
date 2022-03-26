abe501b6d3db4b0e2e8ebb0b18db2691a73462f8
Kari Cheslock <karisd5274@gmail.com>
2022-03-26 15:11:27 -0700

    Initial version for production

--- src/lib/firebase-config.js
+++ src/lib/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_AP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDqxuDJHmVvHw0BAMaymCvN2AkNpJ39aQw",
+  authDomain: "reddit-clone-65881.firebaseapp.com",
+  projectId: "reddit-clone-65881",
+  storageBucket: "reddit-clone-65881.appspot.com",
+  messagingSenderId: "490768746550",
+  appId: "1:490768746550:web:27e8d6d1c978daa943b097"
 };
 
 // Initialize Firebase
